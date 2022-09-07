e40bf733c36feff551c841b371cf108cd4f002a5
mahmoud salem <mahmudsalem.eg@gmail.com>
2022-09-07 23:07:49 +0200

    intros

--- app/Repositories/Dashboard/Setting/IntroRepository.php
+++ app/Repositories/Dashboard/Setting/IntroRepository.php
@@ -11,12 +11,12 @@ class IntroRepository
     // get Intros and create datatable data.
     public static function getIntrosData(array $data)
     {
-        $faqs = Intro::orderBy('id' , 'DESC')->get();
-        return DataTables::of($faqs)
-            ->addColumn('actions', function ($faq) {
-                $ul = '<a data-toggle="tooltip" title="' . trans('admin.edit') . '" id="' . $faq->id . '" onclick="editIntro(this);return false;" href="#" class="on-default edit-row btn btn-info"><i data-feather="edit"></i></a>
+        $intros = Intro::orderBy('id' , 'DESC')->get();
+        return DataTables::of($intros)
+            ->addColumn('actions', function ($intro) {
+                $ul = '<a data-toggle="tooltip" title="' . trans('admin.edit') . '" id="' . $intro->id . '" onclick="editIntro(this);return false;" href="#" class="on-default edit-row btn btn-info"><i data-feather="edit"></i></a>
                    ';
-                $ul .= '<a data-toggle="tooltip" title="' . trans('admin.delete_action') . '" id="' . $faq->id . '" onclick="deleteIntro(this);return false;" href="#" class="on-default remove-row btn btn-danger"><i data-feather="delete"></i></a>';
+                $ul .= '<a data-toggle="tooltip" title="' . trans('admin.delete_action') . '" id="' . $intro->id . '" onclick="deleteIntro(this);return false;" href="#" class="on-default remove-row btn btn-danger"><i data-feather="delete"></i></a>';
                 return $ul;
             })->make(true);
     }
@@ -25,14 +25,14 @@ class IntroRepository
     {
         $intros = Intro::orderBy('id' , 'DESC')->count();
         if($intros < 3){
-            $faqData = [
+            $introData = [
                 'title_ar' => $data['title_ar'],
                 'title_en' => $data['title_en'],
                 'description_ar' => $data['description_ar'],
                 'description_en' => $data['description_en'],
             ];
 
-            $created = Intro::create($faqData);
+            $created = Intro::create($introData);
             if ($created) {
                 return true;
             }
@@ -42,9 +42,9 @@ class IntroRepository
 
     public static function deleteIntro(array $data)
     {
-        $faq = Intro::where(['id' => $data['id']])->first();
-        if ($faq) {
-            $faq->forceDelete();
+        $intro = Intro::where(['id' => $data['id']])->first();
+        if ($intro) {
+            $intro->forceDelete();
             return true;
         }
         return false;
@@ -52,24 +52,24 @@ class IntroRepository
 
     public static function getIntroData(array $data)
     {
-        $faq = Intro::where(['id' => $data['id']])->first();
-        if ($faq) {
-            return $faq;
+        $intro = Intro::where(['id' => $data['id']])->first();
+        if ($intro) {
+            return $intro;
         }
         return false;
     }
 
     public static function editIntro(array $data)
     {
-        $faq = Intro::where(['id' => $data['id']])->first();
-        if ($faq) {
-            $faqData = [
+        $intro = Intro::where(['id' => $data['id']])->first();
+        if ($intro) {
+            $introData = [
                 'title_ar' => $data['title_ar'],
                 'title_en' => $data['title_en'],
                 'description_ar' => $data['description_ar'],
                 'description_en' => $data['description_en'],
             ];
-            $updated = $faq->update($faqData);
+            $updated = $intro->update($introData);
             if ($updated) {
                 return true;
             }
