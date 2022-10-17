2b4dc993a5d252388615fd728a434501660ea431
mahmoud salem <mahmudsalem.eg@gmail.com>
2022-10-17 21:29:38 +0200

    -

--- app/Http/Controllers/Api/Setting/SettingController.php
+++ app/Http/Controllers/Api/Setting/SettingController.php
@@ -28,7 +28,7 @@ class SettingController extends Controller
 //            return  response()->json([$result]);
 
         } catch (PDOException $e) {
-            echo $e->getTraceAsString();
+            print_r($e->getTrace());
         }
         die();
 
